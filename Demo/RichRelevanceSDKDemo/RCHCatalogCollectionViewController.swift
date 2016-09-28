46eee974017a92ba42013ba14d045e1676afda9b
Ariana Antonio <arianarantonio@gmail.com>
2016-09-28 15:44:31 -0700

    RRF-30: Catalog Screen New - Minor tweaks

--- Demo/RichRelevanceSDKDemo/RCHCatalogCollectionViewController.swift
+++ Demo/RichRelevanceSDKDemo/RCHCatalogCollectionViewController.swift
@@ -15,17 +15,15 @@ class RCHCatalogCollectionViewController: UICollectionViewController {
     @IBOutlet var spinner: UIActivityIndicatorView!
     
     var productArray: [RCHRecommendedProduct] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       collectionView!.register(UINib(nibName: "RCHProductCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
-        loadData()
+        
+        collectionView!.register(UINib(nibName: "RCHProductCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
     }
     
     func loadData() {
@@ -39,38 +37,41 @@ class RCHCatalogCollectionViewController: UICollectionViewController {
         
         RCHSDK.defaultClient().sendRequest(strategyBuilder.build(), success: { (responseObject) in
             
-            let strategyResponse: RCHStrategyResult = responseObject as! RCHStrategyResult
-            self.productArray = strategyResponse.recommendedProducts!
+            guard let strategyResponse = responseObject as? RCHStrategyResult else {
+                print("Result Error")
+                return
+            }
+            guard let productArray = strategyResponse.recommendedProducts else {
+                print("Recommended Products Error")
+                return
+            }
+            self.productArray = productArray
             
             self.collectionView?.reloadData()
             self.spinner.stopAnimating()
-            self.spinner.isHidden = true
-            
         }) { (responseObject, error) in
             print(error)
         }
     }
     
     @IBAction func menuSelected(sender: UIBarButtonItem) {
-        
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-        self.present(storyboard.instantiateViewController(withIdentifier: "rootMenu"), animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
-
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return productArray.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! RCHProductCollectionViewCell
-        let product: RCHRecommendedProduct = productArray[indexPath.row]
-
+        let product = productArray[indexPath.row]
+        
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .currency
         numberFormatter.string(from: product.priceCents!)
