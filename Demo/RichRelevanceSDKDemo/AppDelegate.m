f2a422df2a8b94b45e96d6277fc61a62932ca6fd
Brian King <brian.king@raizlabs.com>
2016-11-08 14:09:49 -0500

    Update app delegate

--- Demo/RichRelevanceSDKDemo/AppDelegate.m
+++ Demo/RichRelevanceSDKDemo/AppDelegate.m
@@ -49,7 +49,7 @@
     // Create a configuration and use it to configure the default client.
     RCHAPIClientConfig *config = [[RCHAPIClientConfig alloc] initWithAPIKey:apiKey
                                                                APIClientKey:apiClientKey
-                                                                   endpoint:RCHEndpointIntegration
+                                                                   endpoint:RCHEndpointProduction
                                                                    useHTTPS:YES];
     config.APIClientSecret = @"r5j50mlag06593401nd4kt734i";
     config.userID = userID;
